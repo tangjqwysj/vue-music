af42ea30c9427dee6eb33ba1ceb027c3cfc57f85
tangjqwysj <tangjqwysj@sina.com>
2019-09-04 14:03:31 +0800

    recommend,singer界面已做好

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './store/store'
+import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
+
+import './common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('./common/image/default.png')
+})
 
 Vue.config.productionTip = false
 
